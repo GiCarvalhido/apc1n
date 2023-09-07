9ddb5e5764a2aed463c2cc8d695818845e6bd22e
carvalhido <142457693+carvalhido@users.noreply.github.com>
2023-09-07 00:52:05 +0000

    Aula 4

---
+++ praticas/pratica01/questao03.c
@@ -0,0 +1,9 @@
+/*
+Faça um programa em C que calcule o perímetro de uma pizza (P = 2 x PI x r). Considere o raio um número inteiro e a constante PI igual a 3,1416.
+*/
+#include <stdio.h>
+
+int main() {
+
+  return 0;
+}
